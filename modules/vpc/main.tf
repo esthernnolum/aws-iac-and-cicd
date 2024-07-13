301e7cbdbe990116dfc23daea55717c676cd80cc
Nnolum Esther <esthernnolum@gmail.com>
2024-07-13 14:29:05 +0100

    update architecture diagram

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name    = "my-vpc"
   cidr    = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24"]
 
